2cd79268a775eb90e5d771fec628d3fb050bf570
renanamr <56835236+renanamr@users.noreply.github.com>
2020-02-07 18:42:58 +0000

    Add files via upload

---
+++ app/generated/source/buildConfig/debug/com/renan/notas_diarias/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.renan.notas_diarias;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.renan.notas_diarias";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
